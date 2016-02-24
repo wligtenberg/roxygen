d4df26b14e0473bf11532293aa8b0308a41ed1cf
Willem Ligtenberg <willem@wligtenberg.nl>
2016-02-24 20:05:39 +0000

    uodated documentation

---
+++ vignettes/quickcheck.R
@@ -0,0 +1,15 @@
+## ---- include = FALSE----------------------------------------------------
+knitr::opts_chunk$set(comment = "#>", collapse = TRUE)
+
+## ---- eval = FALSE-------------------------------------------------------
+#  #' Add one to a numeric value
+#  #' @param x numeric
+#  #' @return numeric x+1
+#  #' @author Willem Ligtenberg
+#  #' @export
+#  #' @name plus_1
+#  #' @quickcheck plus_1(x) == x+1
+#  plus_1 <- function(x){
+#    return(x+1)
+#  }
+
